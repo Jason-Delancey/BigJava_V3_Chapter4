0db0cbe6808c682c5e966f637a2fc3668209fa0f
Jason Delancey <jason.r.delancey@gmail.com>
2016-01-12 01:11:57 -0500

    completed p4.5

---
+++ src/DataSetTester.java
@@ -0,0 +1,33 @@
+
+
+/**
+ * 
+ */
+
+/**
+ * @author CloudStrife
+ *
+ */
+public class DataSetTester
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		DataSet test = new DataSet();
+		test.addValue(5);
+		test.addValue(12);
+		test.addValue(32);
+		test.addValue(17);
+		
+		System.out.println(test.getSum());
+		System.out.println("Expected result: 66");
+		
+		System.out.println(test.getAverage());
+		System.out.println("Expected result: 16.5");
+	}
+
+}
